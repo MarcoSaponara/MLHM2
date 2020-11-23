035db241c1b1a96ab5ea7936de2790500352b830
MarcoSaponara <62574546+MarcoSaponara@users.noreply.github.com>
2020-11-23 16:23:52 +0100

    Update caltech_dataset.py

--- caltech_dataset.py
+++ caltech_dataset.py
@@ -18,10 +18,10 @@ class Caltech(VisionDataset):
     def __init__(self, root, split='train', transform=None, target_transform=None):
         super(Caltech, self).__init__(root, transform=transform, target_transform=target_transform)
 
-        self.split = split # This defines the split you are going to use
-                           # (split files are called 'train.txt' and 'test.txt')
-
- 
+        self.split = split 
+	# This defines the split you are going to use
+        # (split files are called 'train.txt' and 'test.txt')
+        
 	paths = []
 	with open(split + '.txt', 'r') as f:
 	    for l in lines:
